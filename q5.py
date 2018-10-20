1e8e494e208167c0023a2bf5ff485c87ce883416
anthoxo <AnthonyNjiva@free.fr>
2018-10-20 16:47:35 +0200

    q5 v1

--- q5.py
+++ q5.py
@@ -1,8 +1,16 @@
 # ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
 
-import numpy as np
+def rec_foundsum(n, tab, aux):
+  result = []
+  if (n == 0):
+    return [aux]
+  for i in range(len(tab)):
+    if (tab[i] <= n):
+      result += rec_foundsum(n-tab[i], tab, aux + 1)
+  return result
+        
 
 def question05(allowedAllocations, totalValue):
   # modify and then return the variable below
-  answer = -1
-  return answer
+  result = rec_foundsum(totalValue, allowedAllocations, 0)
+  return min(result)
