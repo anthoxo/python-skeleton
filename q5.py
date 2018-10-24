aab431a8679a813cf47a57b12f3f0e7b371fc4ea
anthoxo <AnthonyNjiva@free.fr>
2018-10-24 15:40:04 +0200

    5 patch for division 0

--- q5.py
+++ q5.py
@@ -10,7 +10,7 @@ def question05(allowedAllocations, totalValue):
     resultTmp = [l[j]]
     while (j < len(l)) and (sum(resultTmp) < n):
       checkMod = checkModulo(n - sum(resultTmp), l)
-      if (checkMod != -1):
+      if (checkMod > 0):
         if (answer == -1 or answer > len(resultTmp)+(n - sum(resultTmp))//checkMod):
           answer = len(resultTmp)+(n - sum(resultTmp))//checkMod
       if sum(resultTmp) + l[j] <= n:
