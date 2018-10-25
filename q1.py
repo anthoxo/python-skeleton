859494045a0420d8dfd187a690501dc975b14470
anthoxo <AnthonyNjiva@free.fr>
2018-10-25 21:50:34 +0200

    1 fnozfz

--- q1.py
+++ q1.py
@@ -1,64 +1,96 @@
 # # ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
+import cProfile
+
+class BinaryTree:
+  left = None
+  right = None
+
+  def __init__(self):
+    _ = 0
+    
+  def addNode(self, number):
+    if (len(number) != 0):
+      if (number[0] == 0):
+        if (self.left == None):
+          self.left = BinaryTree()
+        self.left = self.left.addNode(number[1:])
+      else:
+        if (self.right == None):
+          self.right = BinaryTree()
+        self.right = self.right.addNode(number[1:])
+    return self
+  
+  def isInTree(self, number):
+    if (len(number) == 0):
+      return True
+    else:
+      if (number[0] == 0):
+        if (self.left == None):
+          return False
+        else:
+          return self.left.isInTree(number[1:])
+      else:
+        if (self.right == None):
+          return False
+        else:
+          return self.right.isInTree(number[1:])
+
+  def maxXor(self, number):
+    if (len(number) == 0):
+      return []
+    else:
+      if (number[0] == 0):
+        if (self.right == None):
+          return [0] + self.left.maxXor(number[1:])
+        else:
+          return [1] + self.right.maxXor(number[1:])
+      else:
+        if (self.left == None):
+          return [0] + self.right.maxXor(number[1:])
+        else:
+          return [1] + self.left.maxXor(number[1:])
+  
+class Number:
+  n = 0
+  b = []
+
+  def __init__(self, n):
+    if(isinstance(n, int)):
+      self.n = n
+      self.setBinary(n)
+    elif (isinstance(n, list)):
+      self.b = n
+      self.setNumber(n)
+  
+  def getNumber(self):
+    return self.n
+  def getBinary(self):
+    return self.b
+  
+  def setNumber(self, b):
+    r = 0
+    for i in range(16):
+      r += b[i] * 2**(15-i)
+    self.n = r
+  
+  def setBinary(self, n):
+    tmp = bin(n)[2:]
+    self.b = [0 for _ in range(16-len(tmp))] + [int(tmp[i]) for i in range(len(tmp))]
 
 # # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
   answer = 0
-  l = sorted(portfolios, reverse = True)
-  if (len(l) < 2 or max(l) == 0):
-    return answer
+  tree = BinaryTree()
+  tabIter = [Number(portfolios[i]) for i in range(len(portfolios))]
 
-  if min(l) == max(l):
-    return answer
-  
-  bitFort = bitPoidsFort(l)
-  n = bitFort
-  ind = 0
-  while(bitFort != 0):
-    j = swapping(ind, l[ind:], bitFort)
-    if (j == -1):
-      bitFort -= 1
-    else:
-      if (j != ind):
-        tmp = l[j]
-        l[j] = l[ind]
-        l[ind] = tmp
-      l = xoring(ind, l, bitFort)
-      bitFort -= 1
-      ind += 1
-  return xor_result(l, n)
+  for i in range(len(tabIter)):
+    tmp = tabIter[i]
+    tree.addNode(tmp.getBinary())
 
-def swapping(indDeb, tab, bitFort):
-  for i in range(indDeb, len(tab)):
-    if tab[i] >= 2**bitFort:
-      return i
-  return -1
-
-def xoring(ind, tab, bitFort):
-  for i in range(len(tab)):
-    if (i != ind):
-      t1 = [int(i) for i in bin(tab[i])[2:]]
-      t = [0 for i in range(16 - len(t1))] + t1
-      if (t[15-bitFort] == 1):
-        tab[i] = tab[i] ^ tab[ind]      
-  return tab
-
-def xor_result(tab, n):
-  result = tab[0]
-  for i in range(1, len(tab)):
-    result = result ^ tab[i]
-  return result
-
-def bitPoidsFort(tab):
-  return sum([1 for i in range(16) if max(tab) - 2**i >= 0])-1
-
-def question01bis(portfolios):
-  # modify and then return the variable below
-  answer = 0
-  if len(portfolios) < 2 or max(portfolios) == 0:
-    return answer
-  answer = max([portfolios[i] ^ portfolios[j] for i in range(len(portfolios)-1) for j in range(i+1, len(portfolios))])
+  for i in range(len(tabIter)):
+    tmp = tabIter[i]
+    t = Number(tree.maxXor(tmp.getBinary()))
+    if (answer < t.getNumber()):
+      answer = t.getNumber()
   return answer
-
-print(question01([i for i in range(1000)]))
-print(question01bis([i for i in range(1000)]))
