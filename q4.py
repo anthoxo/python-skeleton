d7943a1a4cdaa3ca6ec86ba8ad872ea92461ef76
anthoxo <AnthonyNjiva@free.fr>
2018-10-26 02:24:57 +0200

    4 uzbfninzf

--- q4.py
+++ q4.py
@@ -5,8 +5,14 @@ def question04(rows, numberMachines):
   n = int(numberMachines)
   result = -1
   for tab in rows:
-    for i in range(0, len(tab)-n):
-      if ('X' not in tab[i:i+n]):
-        t = sum(tab[i:i+n])
-        result = t if result == -1 else min(result, t)
+    tmp = [] 
+    for i in range(0, len(tab)):
+      if tab[i] == "X":
+        tmp = []
+      else:
+        tmp += [int(tab[i])]
+        if (len(tmp) == n):
+          t = sum(tmp)
+          result = t if result == -1 else min(result, t)
+          tmp = tmp[1:]
   return 0 if result == -1 else result
